13ff3c2a19437f03b142ce38704884418a84dd0a
Rizal Suryawan <rizalsuryawan86@gmail.com>
2023-10-06 13:53:15 +0700

    Updates

--- _app/immutable/chunks/config.13957b55.js
+++
@@ -1,1 +0,0 @@
-const o="rizal s",t="homepage portfolio web",a="rizal suryawan";export{a as c,t as d,o as t};
---
+++ _app/immutable/chunks/config.39b02c5b.js
@@ -0,0 +1,1 @@
+const t="rizal s",s="build with love ❤",a="rizal suryawan";export{a as c,s as d,t};
