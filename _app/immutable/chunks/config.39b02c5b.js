111e5af724c76847165806acdb15b03d2c5c31f4
rizal suryawan <rizalsuryawan86@gmail.com>
2023-12-10 03:07:34 +0700

    Updates

--- _app/immutable/chunks/config.39b02c5b.js
+++
@@ -1,1 +0,0 @@
-const t="rizal s",s="build with love ❤",a="rizal suryawan";export{a as c,s as d,t};
---
+++ _app/immutable/nodes/6.326af896.js
@@ -0,0 +1,10 @@
+import{s as o,n as i}from"../chunks/scheduler.b0c1c2c3.js";import{S as a,i as n,g as l,h as c,x as m,k as d,a as p,f as h}from"../chunks/index.b438a05c.js";function b(r){let e,s=`<h1 class="mb-2 text-5xl font-bold text-rose-600">Welcome to showcase</h1> <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur aliquid ut hic necessitatibus
+		modi. Aspernatur sunt iste harum rerum quos similique libero! Accusantium vel labore dolores
+		modi tenetur quaerat voluptatibus? Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+		Tenetur aliquid ut hic necessitatibus modi. Aspernatur sunt iste harum rerum quos similique
+		libero! Accusantium vel labore dolores modi tenetur quaerat voluptatibus? Lorem ipsum, dolor sit
+		amet consectetur adipisicing elit. Tenetur aliquid ut hic necessitatibus modi. Aspernatur sunt
+		iste harum rerum quos similique libero! Accusantium vel labore dolores modi tenetur quaerat
+		voluptatibus? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur aliquid ut hic
+		necessitatibus modi. Aspernatur sunt iste harum rerum quos similique libero! Accusantium vel
+		labore dolores modi tenetur quaerat voluptatibus?</p>`;return{c(){e=l("section"),e.innerHTML=s,this.h()},l(t){e=c(t,"SECTION",{class:!0,"data-svelte-h":!0}),m(e)!=="svelte-16rpk00"&&(e.innerHTML=s),this.h()},h(){d(e,"class","rounded-xl border border-[#2c2c2c] bg-[#212121] px-4 py-3 text-white shadow shadow-black/25")},m(t,u){p(t,e,u)},p:i,i,o:i,d(t){t&&h(e)}}}class x extends a{constructor(e){super(),n(this,e,null,b,o,{})}}export{x as component};
