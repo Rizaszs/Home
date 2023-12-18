6a5a888e2384dff621747576bf3d4e2037d0de68
rizal suryawan <rizalsuryawan86@gmail.com>
2023-12-18 11:50:23 +0700

    feat: update ajah

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,5 @@
 <section class="section-container">
-	<h1 class=" mb-2 text-5xl font-bold text-teal-600">Welcome to my fucking about</h1>
+	<h1 class=" mb-2 text-5xl font-bold text-teal-600">Welcome to my about</h1>
 	<p>
 		Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur at laborum nostrum maxime
 		veniam exercitationem quis ducimus possimus eum praesentium sit, cumque soluta mollitia a. Iste
